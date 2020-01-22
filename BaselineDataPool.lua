fcbc6a2ab893b98727d333c73a3bb5c013630ced
Aseem Prashar <prasharaseem@gmail.com>
2020-01-22 03:05:46 -0600

    Add file BaselineDataPool to establish the highest baseline. Demonstrate accuracy when all the data is pooled together

---
+++ BaselineDataPool.lua
@@ -0,0 +1,63 @@
+--[[BaselineOneUSer
+
+In this file we try to crreate a baseline scenario. There will be only one user that will train on all the samples in the entire Training Dataset.
+
+The accuracy obtained will represent the baseline as highest accuracy.
+
+-Create One user
+-Train data is entire dataset
+-Train
+-Test Accuracy
+--]]
+
+require 'nn'
+require 'torch'
+require 'dataset-mnist'
+require 'image'
+require 'optim'
+require 'Helper'
+
+--Basic Parameters
+batchSize= 10
+learningRate=0.01
+weightDecay=1e-7
+NumParticipant=1
+epoch=2
+
+torch.manualSeed(46)
+
+classes = {'1','2','3','4','5','6','7','8','9','10'}
+geometry = {32,32}
+
+--loss Criterion 
+criterion=nn.ClassNLLCriterion()
+
+--Dataset- MNIST
+nbTrainingPatches = 60000
+nbTestingPatches = 10000
+
+--Adding helpers to avoid tripping errors in Helper
+trainLogger = optim.Logger(paths.concat("logs", 'train.log'))
+testLogger = optim.Logger(paths.concat("logs", 'test.log'))
+RefUserLogger=optim.Logger(paths.concat("logs",'RefUserLogger.log'))
+ServerLogger=optim.Logger(paths.concat("logs",'ServerLogger.log'))
+
+-- Confusion Matrix
+confusion = optim.ConfusionMatrix(classes)
+
+
+-- create test sets and normalize
+testData = mnist.loadTestSet(nbTestingPatches, geometry)
+testData:normalizeGlobal(mean, std)
+
+
+trainData = mnist.loadTrainSet(nbTrainingPatches, geometry, 1)
+trainData:normalizeGlobal(mean, std)
+
+LoneUser=CreateNN()
+
+helper.train(trainData, LoneUser)
+
+helper.test(testData, LoneUser)
+
+--
